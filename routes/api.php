8c2e1a5eaf56aa7ebc7cf7895b8250906e6198f9
Cristian <carincon93@gmail.com>
2025-12-24 11:41:42 -0500

    feat: Add login feature test and group authentication routes under `/auth` prefix.

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,17 @@ use App\Roles\RoleController;
 Route::prefix(config('app.api_prefix'))->group(function () {
     Route::middleware('throttle:global')->group(function () {
 
-        Route::post('register', [AuthController::class, 'register']);
-        Route::post('login', [AuthController::class, 'login']);
-        Route::post('refresh', [AuthController::class, 'refresh']);
+        Route::prefix('auth')->group(function () {
+            Route::post('register', [AuthController::class, 'register']);
+            Route::post('login', [AuthController::class, 'login']);
+            Route::post('refresh', [AuthController::class, 'refresh']);
+        });
 
         Route::middleware('auth:api')->group(function () {
-            Route::post('profile', [AuthController::class, 'profile']);
-            Route::post('logout', [AuthController::class, 'logout']);
+            Route::prefix('auth')->group(function () {
+                Route::post('profile', [AuthController::class, 'profile']);
+                Route::post('logout', [AuthController::class, 'logout']);
+            });
 
             Route::middleware(EnsureUserHasRole::class . ':admin')->group(function () {
                 Route::apiResource('users', UserController::class);
