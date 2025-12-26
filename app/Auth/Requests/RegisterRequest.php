dcf201acf52803604957583703f7ccaaa38c93c5
Cristian <carincon93@gmail.com>
2025-12-26 17:48:53 -0500

    feat: Add user registration functionality and update login request validation rule syntax.

--- app/Auth/Requests/RegisterRequest.php
+++ app/Auth/Requests/RegisterRequest.php
@@ -4,7 +4,7 @@ namespace App\Auth\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class LoginRequest extends FormRequest
+class RegisterRequest extends FormRequest
 {
     public function authorize(): bool
     {
@@ -14,6 +14,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'username' => ['required', 'max:255'],
             'email' => ['required', 'email', 'max:255'],
             'password' => ['required', 'string', 'max:255'],
         ];
